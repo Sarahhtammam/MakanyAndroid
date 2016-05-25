323ff06ca6a672cc20c6993b71415ea812df4e3b
heba <heba.khazbak@gmail.com>
2016-05-25 15:04:26 +0200

    GPSTracker

--- AndroidMakany/src/com/androidActivities/TestActivity.java
+++ AndroidMakany/src/com/androidActivities/TestActivity.java
@@ -5,12 +5,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
-import com.controllers.Application;
-import com.controllers.EventController;
+import com.controllers.GPSTracker;
 
 public class TestActivity extends Activity implements OnClickListener  {
-
+	
+	GPSTracker gps;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -24,9 +25,27 @@ public class TestActivity extends Activity implements OnClickListener  {
 	@Override
 	public void onClick(View v) 
 	{
-		Application.setUserEmail("magieda@hotmail.com");
+		/*Application.setUserEmail("magieda@hotmail.com");
 		EventController eventController = new EventController();
-			eventController.getGoingEvents("magiedaa@hotmail.com", "");
+			eventController.getGoingEvents("magiedaa@hotmail.com", "");*/
+		
+		 // create class object
+        gps = new GPSTracker(this);
+
+        // check if GPS enabled     
+        if(gps.canGetLocation()){
+             
+            double latitude = gps.getLatitude();
+            double longitude = gps.getLongitude();
+             
+            // \n is for new line
+            Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();    
+        }else{
+            // can't get location
+            // GPS or Network is not enabled
+            // Ask user to enable GPS/network in settings
+            gps.showSettingsAlert();
+        }
 	}
 	
 }
